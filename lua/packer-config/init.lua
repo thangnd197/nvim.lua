0b23a8541e289a86efcae3f69a4055a47ba57b79
Nguyen Dinh Thang - DE <thang.nd2@citigo.com.vn>
2022-11-07 10:08:17 +0700

    fix treesisters run TSUpdate

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -9,8 +9,12 @@ return require("packer").startup(function(use)
   use "hrsh7th/cmp-nvim-lsp" -- LSP source for nvim-cmp
   use "saadparwaiz1/cmp_luasnip" -- Snippets source for nvim-cmp
   use "L3MON4D3/LuaSnip" -- Snippets plugin
-  use {"nvim-treesitter/nvim-treesitter",
-       run = ":TSUpdate",
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      run = function()
+          local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+          ts_update()
+      end,
   }
   use "nvim-lua/plenary.nvim"
   use {
